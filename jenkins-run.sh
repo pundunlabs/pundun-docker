36af91480530e5118eb7e2cb6383ec1eb11e5445
erdem <erdem.aksu@mobilearts.com>
2016-12-20 15:33:24 +0100

    Run docker build if image does not exists locally.

--- jenkins-run.sh
+++ jenkins-run.sh
@@ -10,20 +10,8 @@ _dockerize(){
     docker push pundunlabs/pundun-$1:$2
 }
 
-echo 'Starting pipeline by' `whoami`
-REPOSRC=https://github.com/pundunlabs/pundun-docker.git
-LOCALREPO=pundun-docker
-LOCALREPO_VC_DIR=$LOCALREPO/.git
-
-if [ ! -d $LOCALREPO_VC_DIR ]; then
-    git clone $REPOSRC $LOCALREPO
-else
-    (cd $LOCALREPO && git pull $REPOSRC)
-fi
-cd $LOCALREPO
-
 tag="$(git ls-remote --tags https://github.com/pundunlabs/pundun.git | cut -d "/" -f 3 |grep -v -|grep -v {| sort -n -t. -k1 -k2 -k3 -r | head -n1)"
-if [ ! "$(docker images -q pundunlabs/pundun-$tag:centos-6.7 2> /dev/null)" = "" ]; then
+if [ "$(docker images -q pundunlabs/pundun-$tag:centos-6.7 2> /dev/null)" = "" ]; then
 {
     _dockerize $tag centos-6.7
     _dockerize $tag ubuntu-16.04
