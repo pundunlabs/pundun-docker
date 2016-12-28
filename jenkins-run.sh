3b6381f9969659473c2754977cb003622f196f6b
erdem <erdem.aksu@mobilearts.com>
2016-12-28 11:19:13 +0100

    Add ubuntu 14.04 and centos 7 builds.

--- jenkins-run.sh
+++ jenkins-run.sh
@@ -11,13 +11,21 @@ _dockerize(){
     docker push pundunlabs/pundun-$1:$2
 }
 
+dcleanup(){
+    docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
+    docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
+}
+
 tag="$(git ls-remote --tags https://github.com/pundunlabs/pundun.git | cut -d "/" -f 3 |grep -v -|grep -v {| sort -n -t. -k1 -k2 -k3 -r | head -n1)"
 if [ "$(docker images -q pundunlabs/pundun-$tag:centos-6.7 2> /dev/null)" = "" ]; then
 {
     _dockerize $tag centos-6.7
+    _dockerize $tag centos-7
+    _dockerize $tag ubuntu-14.04
     _dockerize $tag ubuntu-16.04
 }
 else
     echo "image already pulled."
 fi
 #Cleanup dangling images
+dcleanup
